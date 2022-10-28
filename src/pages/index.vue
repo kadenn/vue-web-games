772cef552b4419bb3fb33ff8f653234ce5749c17
kadenn <k.demirhindi@gmail.com>
2022-10-29 02:55:00 +0300

    small fixes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,15 +7,15 @@ import Chicken from "@/assets/chicken.svg";
     <h1>Fun Games</h1>
     <div class="card-layout">
       <div class="card">
-        <div class="card-image">
-          <figure class="image">
-            <Chicken />
-          </figure>
-        </div>
-        <div class="card-content">
+        <figure class="image">
+          <Chicken />
+        </figure>
+        <div>
           <h2>Reaction Time</h2>
           <p>Test your visual reflexes</p>
-          <router-link class="button" to="/reactiontime">Play</router-link>
+          <div class="button-wrapper">
+            <router-link class="button" to="/reactiontime">Play</router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -23,26 +23,7 @@ import Chicken from "@/assets/chicken.svg";
 </template>
 
 <style scoped>
-h1 {
-  font-size: 2rem;
-  margin: 0rem;
-}
-
-P {
-  font-size: 1.2rem;
-}
-
-.card {
-  min-width: 300px;
-  margin-top: 2rem;
-  background-color: rgb(208, 208, 208);
-  border: 2px solid rgb(0, 0, 0);
-  border-radius: 20px;
-  padding: 1rem;
-}
-
-.card-layout {
-  display: flex;
-  flex-direction: column;
+.button-wrapper {
+  margin: 25px;
 }
 </style>
