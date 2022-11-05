66d0eef68d0f10c75120ff360810a6323b312ffc
kadenn <k.demirhindi@gmail.com>
2022-11-05 05:04:15 +0300

    site language updated

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@ import Chicken from "@/assets/chicken.svg";
     class="mx-2 sm:mx-auto max-w-2xl my-4 p-4 bg-stone-200 rounded-lg shadow-lg border-2 border-black flex flex-col text-center"
   >
     <h2 class="text-2xl sm:text-3xl font-semibold text-gray-900 mb-3">
-      Reaction Time Game ⚡
+      Reaksiyon testi ⚡
     </h2>
 
     <figure class="mx-auto p-3">
@@ -18,7 +18,7 @@ import Chicken from "@/assets/chicken.svg";
       to="/reactiontime"
       class="flex justify-center py-2 px-10 mx-auto my-4 text-xl sm:text-3xl font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
     >
-      Play
+      Oyna
     </router-link>
   </div>
 </template>
